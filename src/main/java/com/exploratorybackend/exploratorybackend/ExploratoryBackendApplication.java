2fd9f6e6ba47ea792d8454da871727c821f609ee
Kevin Buckley <kevin.buckley2013@gmail.com>
2020-10-10 12:52:21 -0400

    init commit

---
+++ src/main/java/com/exploratorybackend/exploratorybackend/ExploratoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.exploratorybackend.exploratorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploratoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploratoryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exploratorybackend/exploratorybackend/ExploratoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploratorybackend.exploratorybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploratoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
